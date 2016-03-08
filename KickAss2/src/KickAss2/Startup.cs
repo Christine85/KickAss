5f5adfe1271aaa625705c90ed191d1cd7112688c
enelva <mikael.brunnberg@gmail.com>
2016-03-08 17:10:25 +0100

    Ny kommentar

--- KickAss2/src/KickAss2/Startup.cs
+++ KickAss2/src/KickAss2/Startup.cs
@@ -15,7 +15,7 @@ namespace KickAss2
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //hej monika
+            //hej monika och hej på oss Kick Ass gruppen! vi ska sparka på rumpor nu!
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
