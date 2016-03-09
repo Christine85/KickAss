c1704e890fb2c0bc35b3a3a293a48995f978ef4d
Elias Kronberg <eliaskronberg@gmail.com>
2016-03-09 16:06:38 +0100

    La till CreateUserVM

---
+++ KickAss2/src/KickAss2/ViewModels/CreateUserVM.cs
@@ -0,0 +1,25 @@
+﻿using KickAss2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KickAss2.ViewModels
+{
+    public class CreateUserVM
+    {
+        [Required(ErrorMessage ="Please enter a first name")]
+        
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a last name")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage ="Wrong Format")]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Address> Addresses { get; set; }
+        public List<Order> OrderHistory { get; set; }
+
+    }
+}
