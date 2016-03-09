daa7d29728f351665282c3c8115c09152de984a7
weibust <p.weibust@gmail.com>
2016-03-09 10:25:10 +0100

    Lite struktur tillagt

--- KickAss2/src/KickAss2/Startup.cs
+++ KickAss2/src/KickAss2/Startup.cs
@@ -15,20 +15,20 @@ namespace KickAss2
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //hej monika och hej på oss Kick Ass gruppen! vi ska sparka på rumpor nu!
-			//Ja det ska vi!
-            //Funkar??
+            //var connString = @"Data Source = (localdb)\mssqllocaldb; Initial Catalog = Northwind; Integrated Security = True; Pooling = False";
+            //services.AddEntityFramework()
+            //            .AddSqlServer()
+            //.AddDbContext<NorthwindContext>(o =>
+            //    o.UseSqlServer(connString));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application. asgdhagsda
