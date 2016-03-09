299db690d50e6ff12b3b895a1614eb73cc98f8e7
weibust <p.weibust@gmail.com>
2016-03-09 13:39:27 +0100

    Klasser address, customer, order, products

--- KickAss2/src/KickAss2/Models/Customer.cs
+++ KickAss2/src/KickAss2/Models/Customer.cs
@@ -7,10 +7,17 @@ namespace KickAss2.Models
 {
     public class Customer : User
     {
-        public Customer(int userID, string firstName, string lastName, string email) : base(userID, firstName, lastName, email)
+        public Customer(int userID, string firstName, string lastName, string email, string phoneNumber, List<Address> addresses, List<Product> shoppingCart, List<Order> orderHistory) : base(userID, firstName, lastName, email)
         {
-
+            PhoneNumber = phoneNumber;
+            Addresses = addresses;
+            ShoppingCart = shoppingCart;
+            OrderHistory = orderHistory;
         }
 
+        public string PhoneNumber { get; set; }
+        public List<Address> Addresses { get; set; }
+        public List<Product> ShoppingCart { get; set; }
+        public List<Order> OrderHistory { get; set; }
     }
 }
