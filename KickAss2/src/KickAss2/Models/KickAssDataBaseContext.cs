8bc0a1518c92a8e77b6bd8a850398723c46a2f48
weibust <p.weibust@gmail.com>
2016-03-09 16:29:37 +0100

    lll

---
+++ KickAss2/src/KickAss2/Models/KickAssDataBaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KickAss2.Models
+{
+    public class KickAssDataBaseContext : DbContext
+    {
+        public DbSet<> { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<>().ToTable("");
+    }
+}
+}
+
+/*
+
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().ToTable("Cars");
+        }
+    }
+}
+
+    */
