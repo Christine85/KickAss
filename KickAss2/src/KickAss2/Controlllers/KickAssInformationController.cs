e9076d64d16c9900da538588733c5dd90587cdbe
enelva <mikael.brunnberg@gmail.com>
2016-03-10 14:10:16 +0100

    ContactSida

---
+++ KickAss2/src/KickAss2/Controlllers/KickAssInformationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using KickAss2.ViewModels;
+using Microsoft.AspNet.Mvc.Rendering;
+
+namespace KickAss2.Controlllers
+{
+    public class KickAssInformationController : Controller
+    {
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult GetInTouch()
+        {
+            return View();
+        }
+    }
+}
