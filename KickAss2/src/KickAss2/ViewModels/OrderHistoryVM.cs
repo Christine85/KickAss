bc77046f7e96846215df25084d994853161b7192
weibust <p.weibust@gmail.com>
2016-03-10 10:06:51 +0100

    OrderHistoryVM & CreateUserVM update

---
+++ KickAss2/src/KickAss2/ViewModels/OrderHistoryVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KickAss2.ViewModels
+{
+    public class OrderHistoryVM
+    {
+        [Display(Name = "Order ID")]
+        public int OrderId { get; set; }
+
+        [Display(Name = "Orderdatum")]
+        public DateTime OrderDate { get; set; }
+
+        [Display(Name = "Summa")]
+        public float TotalPrice { get; set; }
+    }
+}
