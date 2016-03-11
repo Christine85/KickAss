301daad01cb1cc3cd2b1880d2ab31a77709ce2a6
enelva <mikael.brunnberg@gmail.com>
2016-03-11 18:10:14 +0100

    Typ en varukorg

---
+++ KickAss2/src/KickAss2/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using KickAss2.ViewModels;
+using Microsoft.AspNet.Mvc.Rendering;
+using KickAss2.Models;
+using Microsoft.AspNet.Http;
+
+namespace KickAss2.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
